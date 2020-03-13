1dc846b64060be88794a4b9ff7138e25eaf00d82
Iker <ikerrivas96@gmail.com>
2020-03-13 13:00:20 +0100

    Create script for extracting info table and generate individual fasta files

---
+++ scripts/create_fasta_and_info_table.py
@@ -0,0 +1,57 @@
+from Bio import AlignIO
+from Bio.AlignIO import MafIO
+import pandas as pd
+import sys
+
+
+# Save the run index
+run = sys.argv[1]
+# Load mafindex
+idx = MafIO.MafIndex('../tmp/filtered.mafindex', '../tmp/filtered.maf', target_seqname)
+# Parse the alignment
+results = idx.search(sys.argv[2], sys.argv[3])
+
+
+# Create an empty dataframe
+df = pd.DataFrame(columns = ['file', 'species', 'chr', 'start', 'gaps'])
+# For each of the alignments
+for i, align in enumerate(results):
+    # Create empty dictionary
+    dct = {'species':[], 'chr':[], 'start':[],'gaps':[]}
+    # Save individual fasta file
+    AlignIO.write(align, '../tmp/run_{}/fasta_{}.fa'.format(run, i), "fasta")
+    # For each of the records
+    for record in align:
+        # Retrieve species
+        dct['species'].append(record.name.split('.')[0])
+        # Retrieve chromosome/contig
+        dct['chr'].append('.'.join(record.name.split('.')[1:]))
+        # Retrieve start coordinate
+        dct['start'].append(record.annotations['start'])
+        # Retrieve gaps encoded in a binary format
+        dct['gaps'].append(''.join([str(0) if n=='-' else str(1) for n in record.seq]))
+    # Convert dictionary to data frame
+    file_df = pd.DataFrame.from_dict(dct)
+    # Insert column mapping to the file
+    file_df.insert(0, 'file', i, True)
+    # Append rows to overall data frame
+    df = df.append(file_df)
+
+# Save the csv file
+df.to_csv('../tmp/info_tables/run_{}.csv'.format(run), index=False)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
