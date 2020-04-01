f67e3c1166e6d52e1c149bead68d8c01ae126ef0
Iker <54534843+rivasiker@users.noreply.github.com>
2020-04-01 14:59:12 +0200

    Add target_seqname as key and re-encode for loop

--- scripts/create_big_table.py
+++ scripts/create_big_table.py
@@ -1,13 +1,17 @@
 import pandas as pd
 import sys
 import os
+import pickle
 
-store =pd.HDFStore('../results/final_table.HDF', complib='blosc')
+target_seqname = sys.argv[1]
 
+slice_lst = pickle.load(open('../tmp/slice_dct.txt', 'rb'))
 
-for file in os.listdir('../tmp/results/'):
-    df=pd.read_hdf('../tmp/results/'+file)
-    store.append(key='data',value=df,format='t',data_columns=['Homo_sapiens'],
+store = pd.HDFStore('../results/final_table.HDF', complib='blosc')
+
+for run in range(len(slice_lst)):
+    df = pd.read_hdf('../tmp/results/run_{}.HDF'.format(run))
+    store.append(key=target_seqname,value=df,format='t',data_columns=['Homo_sapiens'],
                  min_itemsize={ 'chr.Homo_sapiens' : 30 ,
                                 'chr.Gorilla_gorilla_gorilla': 30,
                                 'chr.Pan_troglodytes': 30,
