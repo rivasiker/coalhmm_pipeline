11f3ffede37c7354ef7266189cc8e5eec432be58
MarjolaineRousselle <46812822+MarjolaineRousselle@users.noreply.github.com>
2020-04-07 10:23:06 +0200

    Update to maffilter_controlfile_generation.sh
    - Add option to filter out blocks of 500 bp containing more than 20% gaps over all species

--- scripts/maffilter_controlfile_generation.sh
+++ scripts/maffilter_controlfile_generation.sh
@@ -13,5 +13,11 @@ echo "input.file=$1" > ../tmp/control_file
 echo "input.file.compression=gzip" >> ../tmp/control_file
 echo "input.format=Maf" >> ../tmp/control_file
 echo "output.log=../tmp/maf_filtering.log" >> ../tmp/control_file
-echo -e "maf.filter=Subset(species=($2, $3, $4, $5), strict=yes, keep=no, remove_duplicates=yes), Merge(species=($2, $3, $4, $5), dist_max=100, rename_chimeric_chromosomes=yes), XFullGap(species=($2, $3, $4, $5)), MinBlockLength(min_length=500), WindowSplit(preferred_size=100000, align=adjust, keep_small_blocks=yes), Output(file=../tmp/filtered.maf, compression=none, mask=no)" >> ../tmp/control_file
-
+echo "maf.filter=Subset(species=($2, $3, $4, $5), strict=yes, keep=no, remove_duplicates=yes), \\" >> ../tmp/control_file
+echo "Merge(species=($2, $3, $4, $5), dist_max=100, rename_chimeric_chromosomes=yes), \\" >> ../tmp/control_file
+echo "XFullGap(species=($2, $3, $4, $5)), \\" >> ../tmp/control_file
+echo "MinBlockLength(min_length=500), \\" >> ../tmp/control_file
+echo "AlnFilter(species=($2, $3, $4, $5), window.size=500, window.step=500, max.gap=0.2, max.ent=0.2, missing_as_gap=yes, relative=yes, file=none), \\" >> ../tmp/control_file  
+echo "MinBlockLength(min_length=500), \\" >> ../tmp/control_file
+echo "WindowSplit(preferred_size=100000, align=adjust, keep_small_blocks=yes), \\" >> ../tmp/control_file
+echo "Output(file=../tmp/filtered.maf, compression=none, mask=no)" >> ../tmp/control_file
